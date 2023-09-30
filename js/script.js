1147c825b1f74ee235df8e9457ed20abe01aa034
zelo <rafaelpazeloinforsato@gmail.com>
2023-09-30 12:34:24 -0300

    n sei mais nada

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const horas = document.querySelector(".horas");
-const minutos = document.querySelector(".minutos");
-const segundos = document.querySelector(".segundos");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
 
 const getTime = () => {
     const date = new Date();
     return {
-        horas: date.getHours(),
-        minutos: date.getMinutes(),
-        segundos: date.getSeconds(),
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
     };
 };
 
 setInterval(() => {
-    const { segundos, minutos, horas } = getTime();
-    segundos.style.transform = `translate(0, -50%) rotate(${segundos * 6}deg)`;
-    minutos.style.transform = `translate(0, -50%) rotate(${minutos * 6}deg)`;
-    horas.style.transform = `translate(0, -50%) rotate(${horas * 30}deg)`;
+    const { seconds, minutes, hours } = getTime();
+    seconds.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`;
+    minutes.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`;
+    hours.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`;
 }, 1000);
